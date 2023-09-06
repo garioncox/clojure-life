e556ed15d8eee09b673fb66cbe3a17f072665e87
garioncox <garion.cox@students.snow.edu>
2023-09-06 15:29:02 -0600

    9-6 in class. Unit tests pass, need to refactor will-live?

--- src/demo.clj
+++ src/demo.clj
@@ -12,57 +12,32 @@
     (map (fn [i] [(+ x (dx i)) (+ y (dy i))]) 
          (range 8))))
 
-;; Todo - is there method overloading?
-(defn living-neighbors-of-cell [cell living]
-  (let [x (first cell), y (second cell)]
-  (count
-   (filter #(contains? (set living) %) 
-           (neighbors-of [x y])))))
-
-(defn living-neighbors-of-cells [[cells] living]
-    (map (partial living-neighbors-of-cell) cells))
+(defn living-neighbors
+  ([cell living]
+   (let [x (first cell), y (second cell)]
+     (count
+      (filter #(contains? (set living) %)
+              (neighbors-of [x y])))))
+  ([living]
+   (fn [cell] (living-neighbors cell living))))
 
 (defn will-live? [[x y] living]
-  (let [neighbors (living-neighbors-of-cell [x y] living)]
-    (if (and (contains? (set living) [x y]) (< 1 neighbors) (> 4 neighbors)) true
+  (let [neighbors (living-neighbors [x y] living)]
+    (if (and (living [x y]) (< 1 neighbors) (> 4 neighbors)) true
         (if (and (not= (contains? (set living) [x y]) true) (= 3 neighbors)) true false))))
 
 (defn next-generation [living]
-  (filter true? (map (fn [cell] will-live? cell living) (distinct (apply concat (map neighbors-of living))))))
-
-(comment
-  ;; (range 8) gives the values for [i]
-  (defn neighbors-of [[x y]]
-    (let [dx [-1 0 1 -1 1 -1 0 1]
-          dy [-1 -1 -1 0 0 1 1 1]]
-      (map (fn [i] [(+ x (dx i)) (+ y (dy i))])
-           (range 8))))
-
-;; Todo - is there method overloading?
-  (defn living-neighbors-of-cell [cell living]
-    (let [x (first cell), y (second cell)]
-      (count
-       (filter #(contains? (set living) %)
-               (neighbors-of [x y])))))
-
-  (defn living-neighbors-of-cells [[cells] living]
-    (map (partial living-neighbors-of-cell) cells))
-
-  (defn will-live? [[x y] living]
-    (let [neighbors (living-neighbors-of-cell [x y] living)]
-      (if (and (contains? (set living) [x y]) (< 1 neighbors) (> 4 neighbors)) true
-          (if (and (not= (contains? (set living) [x y]) true) (= 3 neighbors)) true false))))
-
-  (defn next-generation [living]
-    (filter (fn [cell] (will-live? cell living)) (distinct (apply concat (map neighbors-of living)))))
+  (filter (fn [cell] (will-live? cell living)) (distinct (apply concat (map neighbors-of living)))))
 
 
-  (will-live? [2 2] (neighbors-of [-2 -2]))
-  (will-live? [2 2] #{[2 3] [3 3] [1 2] [1 1]})
-  (will-live? [2 2] #{[2 3] [3 3] [1 2]})
-  (will-live? [2 2] #{[2 3] [3 3]})
-  (will-live? [2 2] #{[2 3]})
 
-  (next-generation [[1 1] [1 2] [3 1] [3 2] [4 2]])
+;;;;; Testing Code ;;;;;
 
-  )
+;; (will-live? [2 2] (neighbors-of [-2 -2]))
+;; (will-live? [2 2] #{[2 3] [3 3] [1 2] [1 1]})
+;; (will-live? [2 2] #{[2 3] [3 3] [1 2]})
+;; (will-live? [2 2] #{[2 3] [3 3]})
+;; (will-live? [2 2] #{[2 3]})
+
+;; (next-generation [[1 1] [1 2] [3 1] [3 2] [4 2]])
+;; (next-generation (set '([1 1] [1 2] [3 1] [3 2] [4 2])))
--- test/demo_test.clj
+++ test/demo_test.clj
@@ -14,19 +14,20 @@
 ;;  [1 2] [ x ]
 ;;  [1 1]       [3 1]]
 (deftest living-neighbors-test
-  (is (= 5 (living-neighbors-of-cell [2 2] #{[2 3] [3 3] [1 2] [1 1] [3 1]}))))
+  (is (= 5 (living-neighbors [2 2] #{[2 3] [3 3] [1 2] [1 1] [3 1]})))
+  (is (= 5 ((living-neighbors #{[2 3] [3 3] [1 2] [1 1] [3 1]}) [2 2]))))
 
 (deftest will-live-test
-  (is (= false (will-live? [2 2] (neighbors-of [-2 -2]))))
+  (is (= false (will-live? [2 2] (set (neighbors-of [-2 -2])))))
   (is (= false (will-live? [2 2] #{[2 3] [3 3] [1 2] [1 1]})))
   (is (= true  (will-live? [2 2] #{[2 3] [3 3] [1 2]})))
-  (is (= false  (will-live? [2 2] #{[2 3] [3 3]})))
+  (is (= false (will-live? [2 2] #{[2 3] [3 3]})))
   (is (= false (will-live? [2 2] #{[2 3]}))))
 
 ;; [x] [] [x] [x] ->  [] [] [x] [x]
 ;; [x] [] [x] [ ]     [] [] [x] [x]
 (deftest next-generation-test
-  (is (= '([4 1] [3 2] [4 2] [3 1]) (next-generation (set '([1 1] [1 2] [3 1] [3 2] [4 2]))))))
+  (is (= '([3 1] [4 1] [3 2] [4 2]) (next-generation (set '([1 1] [1 2] [3 1] [3 2] [4 2]))))))
 
 
 
