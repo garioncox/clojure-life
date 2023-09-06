7afe223859347a247f61665218c1c8f17a2286bc
Garion Cox <gariontc@gmail.com>
2023-09-05 20:36:19 -0600

    9-5

--- src/demo.clj
+++ src/demo.clj
@@ -1,4 +1,68 @@
+;; Game of life rules
+;; 1) A living cell with 2 > neighbors > 3 dies
+;; 2) A dead cell with 3 neighbors comes to life
+;; 3) Any other cell remains in its current state
+
 (ns demo)
 
-(defn add [a b]
-  (+ a b))
+;; (range 8) gives the values for [i]
+(defn neighbors-of [[x y]]
+  (let [dx [-1 0 1 -1 1 -1 0 1]
+        dy [-1 -1 -1 0 0 1 1 1]]
+    (map (fn [i] [(+ x (dx i)) (+ y (dy i))]) 
+         (range 8))))
+
+;; Todo - is there method overloading?
+(defn living-neighbors-of-cell [cell living]
+  (let [x (first cell), y (second cell)]
+  (count
+   (filter #(contains? (set living) %) 
+           (neighbors-of [x y])))))
+
+(defn living-neighbors-of-cells [[cells] living]
+    (map (partial living-neighbors-of-cell) cells))
+
+(defn will-live? [[x y] living]
+  (let [neighbors (living-neighbors-of-cell [x y] living)]
+    (if (and (contains? (set living) [x y]) (< 1 neighbors) (> 4 neighbors)) true
+        (if (and (not= (contains? (set living) [x y]) true) (= 3 neighbors)) true false))))
+
+(defn next-generation [living]
+  (filter true? (map (fn [cell] will-live? cell living) (distinct (apply concat (map neighbors-of living))))))
+
+(comment
+  ;; (range 8) gives the values for [i]
+  (defn neighbors-of [[x y]]
+    (let [dx [-1 0 1 -1 1 -1 0 1]
+          dy [-1 -1 -1 0 0 1 1 1]]
+      (map (fn [i] [(+ x (dx i)) (+ y (dy i))])
+           (range 8))))
+
+;; Todo - is there method overloading?
+  (defn living-neighbors-of-cell [cell living]
+    (let [x (first cell), y (second cell)]
+      (count
+       (filter #(contains? (set living) %)
+               (neighbors-of [x y])))))
+
+  (defn living-neighbors-of-cells [[cells] living]
+    (map (partial living-neighbors-of-cell) cells))
+
+  (defn will-live? [[x y] living]
+    (let [neighbors (living-neighbors-of-cell [x y] living)]
+      (if (and (contains? (set living) [x y]) (< 1 neighbors) (> 4 neighbors)) true
+          (if (and (not= (contains? (set living) [x y]) true) (= 3 neighbors)) true false))))
+
+  (defn next-generation [living]
+    (filter (fn [cell] (will-live? cell living)) (distinct (apply concat (map neighbors-of living)))))
+
+
+  (will-live? [2 2] (neighbors-of [-2 -2]))
+  (will-live? [2 2] #{[2 3] [3 3] [1 2] [1 1]})
+  (will-live? [2 2] #{[2 3] [3 3] [1 2]})
+  (will-live? [2 2] #{[2 3] [3 3]})
+  (will-live? [2 2] #{[2 3]})
+
+  (next-generation [[1 1] [1 2] [3 1] [3 2] [4 2]])
+
+  )
--- test/demo_test.clj
+++ test/demo_test.clj
@@ -2,8 +2,34 @@
   (:require [clojure.test :refer :all]
             [demo :refer :all]))
 
-(deftest test-add
-  (is (= 5 (add 2 3))))
+(deftest neighbors-of-test
+  (is (= '([1 1] [2 1] [3 1]
+           [1 2]       [3 2]
+           [1 3] [2 3] [3 3]) (neighbors-of [2 2])))
+  (is (= '([-3 -3] [-2 -3] [-1 -3]
+           [-3 -2]         [-1 -2]
+           [-3 -1] [-2 -1] [-1 -1]) (neighbors-of [-2 -2]))))
+
+;; [[2 3] [3 3]
+;;  [1 2] [ x ]
+;;  [1 1]       [3 1]]
+(deftest living-neighbors-test
+  (is (= 5 (living-neighbors-of-cell [2 2] #{[2 3] [3 3] [1 2] [1 1] [3 1]}))))
+
+(deftest will-live-test
+  (is (= false (will-live? [2 2] (neighbors-of [-2 -2]))))
+  (is (= false (will-live? [2 2] #{[2 3] [3 3] [1 2] [1 1]})))
+  (is (= true  (will-live? [2 2] #{[2 3] [3 3] [1 2]})))
+  (is (= false  (will-live? [2 2] #{[2 3] [3 3]})))
+  (is (= false (will-live? [2 2] #{[2 3]}))))
+
+;; [x] [] [x] [x] ->  [] [] [x] [x]
+;; [x] [] [x] [ ]     [] [] [x] [x]
+(deftest next-generation-test
+  (is (= '([4 1] [3 2] [4 2] [3 1]) (next-generation (set '([1 1] [1 2] [3 1] [3 2] [4 2]))))))
+
+
+
 
 ;; to run:
 ;; clj -X <file>/<function>
