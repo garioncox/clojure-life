d73d1438e6b9ddd664fc50527a36efe12142e3cb
Garion Cox <gariontc@gmail.com>
2023-09-06 19:04:12 -0600

    Removed a line or two of code

--- src/demo.clj
+++ src/demo.clj
@@ -15,29 +15,16 @@
 (defn living-neighbors
   ([cell living]
    (let [x (first cell), y (second cell)]
-     (count
-      (filter #(contains? (set living) %)
-              (neighbors-of [x y])))))
+     (count (filter living (neighbors-of [x y])))))
   ([living]
    (fn [cell] (living-neighbors cell living))))
 
 (defn will-live? [[x y] living]
   (let [neighbors (living-neighbors [x y] living)]
-    (if (and (living [x y]) (< 1 neighbors) (> 4 neighbors)) true
-        (if (and (not= (contains? (set living) [x y]) true) (= 3 neighbors)) true false))))
+    (if (or
+         (and (living [x y]) (< 1 neighbors) (> 4 neighbors))
+         (and (not= (living [x y]) true) (= 3 neighbors)))
+      true false)))
 
 (defn next-generation [living]
   (filter (fn [cell] (will-live? cell living)) (distinct (apply concat (map neighbors-of living)))))
-
-
-
-;;;;; Testing Code ;;;;;
-
-;; (will-live? [2 2] (neighbors-of [-2 -2]))
-;; (will-live? [2 2] #{[2 3] [3 3] [1 2] [1 1]})
-;; (will-live? [2 2] #{[2 3] [3 3] [1 2]})
-;; (will-live? [2 2] #{[2 3] [3 3]})
-;; (will-live? [2 2] #{[2 3]})
-
-;; (next-generation [[1 1] [1 2] [3 1] [3 2] [4 2]])
-;; (next-generation (set '([1 1] [1 2] [3 1] [3 2] [4 2])))
